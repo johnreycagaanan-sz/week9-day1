09a0e27d99210a977e29c5baafe50597a1de10cc
unknown <john.cagaanan@supportzebra.com>
2022-11-16 10:00:24 +0800

    Completed problems 4 and 5

--- assignment_recursion.js
+++ assignment_recursion.js
@@ -1,16 +1,13 @@
 //! Problem #1
 function reverseString(str, lastIndex = str.length-1, reversedArr = []){
     if(lastIndex < 0) return;
-    // console.log(`THE STRING IS ${str} and the lastIndex is ${lastIndex}`)
     reversedArr.push(str[lastIndex])
-    // console.log(`Reversed array is ${reversedArr}`)
     lastIndex--
     reverseString(str, lastIndex, reversedArr)
-    
 return reversedArr.join('')
 }
 
-// console.log(reverseString('hello'))
+console.log(reverseString('hello'))
 
 
 
@@ -18,16 +15,14 @@ return reversedArr.join('')
 
 function palindrome(str, lastIndex = str.length-1, reversedArr = []){
     if(lastIndex < 0) return;
-    // console.log(`THE STRING IS ${str} and the lastIndex is ${lastIndex}`)
     reversedArr.push(str[lastIndex])
-    // console.log(`Reversed array is ${reversedArr}`)
     lastIndex--
     palindrome(str, lastIndex, reversedArr)
     
 return reversedArr.join('') === str
 }
-// console.log(palindrome('tony'))
-// console.log(palindrome('sos'))
+console.log(palindrome('tony'))
+console.log(palindrome('sos'))
 
 //! Problem #3
 
@@ -38,37 +33,35 @@ const flatTheArray = (arr) => {
         if(typeof arr[i] === 'number'){
             flattenedArr.push(arr[i])
         }else if(typeof arr[i] === 'object'){
-            arr = arr.concat(flatTheArray(arr[i]))
+            flattenedArr = flattenedArr.concat(flatTheArray(arr[i]))
         }
     }
     return flattenedArr
 }
 
-// console.log(flatTheArray([1,2,[3,4],[[5,6],7]]))
-// console.log(flatTheArray([1, 2, 3, [4, 5] ]))
-// console.log(flatTheArray([1, [2, [3, 4], [[5]]]]))
-// console.log(flatTheArray([[1],[2],[3]]))
-// console.log(flatTheArray([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]))
+console.log(flatTheArray([1,2,[3,4],[[5,6],7]]))
+console.log(flatTheArray([1, 2, 3, [4, 5] ]))
+console.log(flatTheArray([1, [2, [3, 4], [[5]]]]))
+console.log(flatTheArray([[1],[2],[3]]))
+console.log(flatTheArray([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]))
 
 //! Problem 4
 
 const capitalizeFirstLetter = (array) => {
-    let newArray =[]
+    if(array.length ===0) return []
 
-    for(let i = 0 ; i < array.length; i++){
-        if(typeof array[i]==='string'){
-            newArray.push(array[i][0].toUpperCase() + array[i].slice(1))
-        } else if(typeof array[i] === 'object'){
-            array = array.concat(capitalizeFirstLetter(array[i]))
-        }
-    }
-    return newArray
+    let letters = array[0].split('')
+    letters[0] = letters[0].toUpperCase();
+    return [letters.join('')].concat(capitalizeFirstLetter(array.slice(1)))
 }
 
-console.log(capitalizeFirstLetter([['hello','world'],'world', 'sample']))
+console.log(capitalizeFirstLetter(['one', 'two']))
 
-// const test = (string) => {
-//     return typeof(string[0])
-// }
+//! Problem 5
 
-// console.log(test('sample'))
+const capitalizeAllLetters = (array) => {
+    if(array.length === 1 ) return [array[0].toUpperCase()]
+    return [array[0].toUpperCase()].concat(capitalizeAllLetters(array.slice(1)))
+}
+
+console.log(capitalizeAllLetters(['tony','kim', 'numba', 'wan']))
